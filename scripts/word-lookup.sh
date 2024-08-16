0ad7ab0fad6e1e94e18c422b41e14f5643f6b625
fux <jan.dick@bdp.org>
2024-08-16 11:33:10 +0200

    Removes unwanted characters

--- scripts/word-lookup.sh
+++ scripts/word-lookup.sh
@@ -1,7 +1,8 @@
 word=$(xclip -o)
+word=${word//[$'-., \t\r\n']}
 res=$(curl -s "https://api.dictionaryapi.dev/api/v2/entries/en_US/$word")
 regex=$'"definition":"\K(.*?)(?=")'
 definitions=$(echo $res | grep -Po "$regex")
 separatedDefinition=$(sed ':a;N;$!ba;s/\n/\n\n/g' <<< "$definitions")
 
-notify-send -u low -t 10000 "$word" "$separatedDefinition"
+notify-send -u low -t 30000 "$word" "$separatedDefinition"
